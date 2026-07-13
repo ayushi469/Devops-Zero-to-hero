3652bab9e2699a3c7e545b168316ccf7cbeabc73
Ayushi Gupta <dibs@Ayushis-MacBook-Air.local>
2026-07-13 23:40:11 +0530

    bash-for-devops

--- bash-for-devops/02-conditionals/permission-checker.sh
+++ bash-for-devops/02-conditionals/permission-checker.sh
@@ -1,12 +1,24 @@
 #!/bin/bash 
 
+# ----------------------------------------------------------
+# File    : file-checker.sh
+# Purpose : Check whether a file exists and if it is readable
+#
+# Concepts Covered:
+# - if, elif, else
+# - File test operators (-f, -r)
+# - Logical AND (&&)
+# - User input using read
+# - Conditional execution
+# ----------------------------------------------------------
 
-echo "check if file is there or not:"
+
+echo "Enter the file:"
 read file
 
 if [ ! -f "$file" ]
 then
-	echo "File doesnt exists"
+	echo "File doesn't exists"
 elif [ -f "$file" ] && [ -r "$file" ]
 then
 	echo "File exists and it has read permission"
