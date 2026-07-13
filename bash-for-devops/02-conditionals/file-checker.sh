5e62ca5a581542497475272b61961d1bf915e722
Ayushi Gupta <dibs@Ayushis-MacBook-Air.local>
2026-07-13 23:36:49 +0530

    bash-for-devops

--- bash-for-devops/02-conditionals/file-checker.sh
+++ bash-for-devops/02-conditionals/file-checker.sh
@@ -1,12 +1,25 @@
 #!/bin/bash 
 
+# ----------------------------------------------------------
+# File    : file-checker.sh
+# Purpose : Check whether a file exists
+#
+# Concepts Covered:
+# - File test operator (-f)
+# - if-else statements
+# - User input using read
+# - Conditional execution
+# ----------------------------------------------------------
 
-echo "Print the filename:"
-read file 
 
-if [ -f file.txt ]
+echo "Enter the file name:"
+read filename
+
+echo 
+
+if [ -f "$filename" ]
 then
-	echo "File exists"
+	echo "File exists."
 else 
-	echo "File doesn't exists"
+	echo "File doesn't exists."
 fi
